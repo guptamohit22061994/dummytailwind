65f3c4bc22f4e645bc4f1e24e9fffa52cdf4eeca
mohitgupta2206 <guptamohit2206@gmail.com>
2023-11-30 12:13:00 +0530

    landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ module.exports = {
   content: ["./src/*.html"],
   theme: {
     extend: {},
+    colors: {
+      primarylight: '#6F7CB2',
+      primarybutton: '#111B47',
+      primarydark: '#091133',
+      primarywhite: '#ffffff',
+      primarygrey: '#5D6970',
+      primaryblue: '#37447E',
+      secondaryblue: '#222F65',
+
+
+    },
+    baground: {
+
+      primarybutton: '#111B47',
+
+
+    },
   },
   plugins: [],
 }
