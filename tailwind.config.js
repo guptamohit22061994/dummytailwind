293fc84ce6256276952057153adcf9b930725a3b
mohitgupta2206 <guptamohit2206@gmail.com>
2023-12-01 20:15:37 +0530

    coustomize

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,22 @@ module.exports = {
       primaryblue: '#37447E',
       secondaryblue: '#222F65',
 
-
     },
     backgroundColor: {
       primarybutton: '#111B47',
       primarybackground: '#E7ECFF',
+      white: '#ffffff',
     },
-
+    backgroundImage: {
+      'footer-texture': "url('/src/images/Rectangle.jpg')",
+    },
+    borderColor: {
+      'border-slate-200': '#CDD1D4',
+      primarydark: '#091133',
+    },
+    fontFamily: {
+      'sans': ['Roboto', 'sans-serif'],
+    },
   },
   plugins: [],
 }
